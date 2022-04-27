fd8c26005c42200378a310a8db2e7d76f0a24933
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-27 16:01:18 -0700

    extra extensions

--- jacdac/main.ts
+++ jacdac/main.ts
@@ -4,16 +4,23 @@ namespace GAME_ZIP64 {
 }
 
 namespace modules {
-
+    /**
+     * The 8x8 LED display
+     */
+    //% fixedInstance whenUsed block="gamezip screen"
+    export const gameZipScreen = new LedClient("gamezip screen?device=self&rows=8&columns=8&variant=matrix")
 }
 
 namespace servers {
     function start() {
         jacdac.productIdentifier = 0x34ef625e
         jacdac.startSelfServers(() => {
+            pins.digitalWritePin(DigitalPin.P0, 0)
             const servers: jacdac.Server[] = [
-                
-            ]
+                new jacdac.LedServer(jacdac.LedVariant.Matrix, 64,
+                    jacdac.LedPixelLayout.RgbGrb,
+                    (pixels, brightness) => light.sendWS2812BufferWithBrightness(pixels, DigitalPin.P0, brightness)
+                )]
             return servers
         })
     }
